17abe05956853816ba10278eb06442273d22d732
Isabel <isabelroldancordoba@hotmail.com>
2023-07-27 13:19:51 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reto03';
+  title = 'Día 4 del reto';
+  url = 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg';
+  /* name = 'Isabel Roldán Córdoba'; */
+
+  cities = ['Barcelona', 'Madrid', 'Córdoba'];
+  name!: string;
 }
