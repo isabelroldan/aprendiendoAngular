59ca08d86bccd4f82104f12ba88405c8b998cd1d
Isabel <isabelroldancordoba@hotmail.com>
2023-07-27 17:44:04 +0200

    Ciclo de vida de un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
   title = 'Día 4 del reto';
   url = 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg';
   /* name = 'Isabel Roldán Córdoba'; */
 
   cities = ['Barcelona', 'Madrid', 'Córdoba'];
   name!: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log('Change ->', changes);
+  }
+
+  ngOnInit(): void {
+      console.log('OnInit -> ');
+  }
+
+  ngOnDestroy(): void {
+      console.log('Destroy');
+  }
 }
